760b52b754e4c7704a43449cab32cb19f44799c9
Crowdbotics <team@crowdbotics.com>
2021-09-01 16:30:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "lively_river_30253Identifier",
+  url: "https://lively_river_30253Identifier.botics.co",
+  api: "https://lively_river_30253Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
